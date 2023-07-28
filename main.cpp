b89968ebda79c72f6ea9ac068c54c867e51ec131
Myy1112 <120002840+Myy1112@users.noreply.github.com>
2023-07-29 01:09:59 +0700

    còn lỗi

--- main.cpp
+++ main.cpp
@@ -7,12 +7,29 @@ int main() {
     inputByFile(username_check, "SignUp.txt");
     inputByFile(weak_password_check, "Weak-Pass.txt");
 
-    Account account;
+  Account account;
     logIn(account, username_check);
 
-    continuePrompt();
+    char changePasswordOption;
+    std::cout << "Do you want to change your password? (y/n): ";
+    std::cin >> changePasswordOption;
 
-    changePassword(account.username, weak_password_check);
+    if (changePasswordOption == 'y' || changePasswordOption == 'Y') {
+        changePassword(account.username, weak_password_check);
+        std::cout << "Password changed successfully!\n";
+    }
+
+    continuePrompt(); 
+
+    char signUpMultipleOption;
+    std::cout << "Do you want to sign up multiple accounts? (y/n): ";
+    std::cin >> signUpMultipleOption;
+
+    if (signUpMultipleOption == 'y' || signUpMultipleOption == 'Y') {
+        signUpMultipleAccounts(username_check);
+    }
+
+    continuePrompt(); 
 
     return 0;
 }
--- menu.h
+++ menu.h
@@ -3,10 +3,15 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-
+#include <algorithm>
 #include "account.h"
 #include "bloom_filter.h"
-
+void saveAccountToDatabase(const std::string& username, const std::string& password);
+bool isValidPassword(const std::string& password, const std::string& username, const std::vector<std::string>& weakPasswords);
+bool isValidPassword(const std::string& password, const std::string& username, const std::vector<std::string>& weakPasswords);
+bool isValidAccount(const std::string& username, const std::string& password,const std::vector<std::string>& existingUsernames,const std::vector<std::string>& weakPasswords);
+void saveAccountToDatabase(const std::string& username, const std::string& password); 
+void signUpMultipleAccounts(BloomFilter* bloom_filter);
 void continuePrompt();
 void logIn(Account& account, BloomFilter* username_check);
 void changePassword(const std::string& username, BloomFilter* weak_password_check);
