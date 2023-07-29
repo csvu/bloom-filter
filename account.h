60d9f2b85b1c6ca59660ace983c97757bf396ba4
Myy1112 <120002840+Myy1112@users.noreply.github.com>
2023-07-29 18:34:31 +0700

    update

--- account.h
+++ account.h
@@ -1,8 +1,13 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 struct Account {
     std::string username;
     std::string password;
 };
+
+bool isValidUsername(const std::string& password, const std::string& username, const std::vector<std::string>& weakPasswords);
+bool isValidPassword(const std::string& password, const std::string& username, const std::vector<std::string>& weakPasswords);
+bool isValidAccount(const std::string& username, const std::string& password, const std::vector<std::string>& existingUsernames,  const std::vector<std::string>& weakPasswords);
--- menu.cpp
+++ menu.cpp
@@ -1,73 +1,4 @@
 #include "menu.h"
-bool isValidUsername(const std::string& password, const std::string& username,
-                     const std::vector<std::string>& weakPasswords) {
-    if (username.length() <= 5 || username.length() >= 10) {
-        return false;
-    }
-
-    if (username.find(' ') != std::string::npos) {
-        return false;
-    }
-
-    if (std::find(weakPasswords.begin(), weakPasswords.end(), password) != weakPasswords.end()) {
-        return false;
-    }
-
-    return true;
-}
-bool isValidPassword(const std::string& password, const std::string& username,
-                     const std::vector<std::string>& weakPasswords) {
-    if (password.length() <= 10 || password.length() >= 20) {
-        return false;
-    }
-
-    if (password.find(' ') != std::string::npos) {
-        return false;
-    }
-
-    bool hasUppercase = false;
-    bool hasLowercase = false;
-    bool hasDigit = false;
-    bool hasSpecialChar = false;
-    for (char c : password) {
-        if (std::isupper(c)) {
-            hasUppercase = true;
-        } else if (std::islower(c)) {
-            hasLowercase = true;
-        } else if (std::isdigit(c)) {
-            hasDigit = true;
-        } else if (std::ispunct(c) || std::isspace(c)) {
-            hasSpecialChar = true;
-        }
-    }
-    if (!(hasUppercase && hasLowercase && hasDigit && hasSpecialChar)) {
-        return false;
-    }
-
-    if (std::find(weakPasswords.begin(), weakPasswords.end(), password) != weakPasswords.end()) {
-        return false;
-    }
-
-    if (password == username) {
-        return false;
-    }
-
-    return true;
-}
-
-bool isValidAccount(const std::string& username, const std::string& password,
-                    const std::vector<std::string>& existingUsernames,
-                    const std::vector<std::string>& weakPasswords) {
-    if (!isValidUsername(password, username, weakPasswords)) {
-        return false;
-    }
-
-    if (!isValidPassword(password, username, weakPasswords)) {
-        return false;
-    }
-
-    return true;
-}
 
 void saveAccountToDatabase(const std::string& username, const std::string& password) {
     std::ofstream outFile("Sign Up.txt", std::ios_base::app);
@@ -125,7 +56,7 @@ void primeMenu() {
 
         switch (choice) {
             case 1:
-                signUp();
+                signUpMultipleAccounts(username_check);
                 break;
             case 2:
                 logIn(account, is_logged_in, username_check);
@@ -148,8 +79,6 @@ void primeMenu() {
         }
     }
 }
-
-void signUp() {}
 
 void continuePrompt(bool& loop_out) {
     while (true) {
